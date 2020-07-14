7dbc49739b5d591f809392e197c7f151156202d8
Denis Timofeev <y719xy@gmail.com>
2020-07-14 16:49:03 +0300

    delete variable filmId

--- src/test/java/ru/netology/repository/AfishaRepositoryTest.java
+++ src/test/java/ru/netology/repository/AfishaRepositoryTest.java
@@ -11,12 +11,12 @@ class AfishaRepositoryTest {
 
     private AfishaRepository repository = new AfishaRepository();
     private AfishaManager manager = new AfishaManager(repository);
-    private Film first = new Film(1, 1, "Бладшот", "боевик");
-    private Film second = new Film(2, 2, "Вперед", "мультфильм");
-    private Film third = new Film(3, 3, "Отель Белград ", "комедия");
+    private Film first = new Film(1, "Бладшот", "боевик");
+    private Film second = new Film(2, "Вперед", "мультфильм");
+    private Film third = new Film(3, "Отель Белград ", "комедия");
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -26,34 +26,34 @@ class AfishaRepositoryTest {
     void shouldSave() {
         Film[] expected = new Film[]{third, second, first};
         Film[] actual = manager.getAll();
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void findAll() {
         Film[] expected = new Film[]{first, second, third};
         Film[] actual = repository.findAll();
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void findById() {
         Film[] expected = new Film[]{second};
         Film[] actual = repository.findById(2);
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void removeById() {
         Film[] expected = new Film[]{first, third};
         Film[] actual = repository.removeById(2);
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void removeAll() {
         Film[] expected = new Film[]{};
         Film[] actual = repository.removeAll();
-        assertArrayEquals(expected,actual);
+        assertArrayEquals(expected, actual);
     }
 }
