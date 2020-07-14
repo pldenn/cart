61d874a93487fdefa01019ab270520b849915afd
Denis Timofeev <y719xy@gmail.com>
2020-07-14 16:30:30 +0300

    delete variable productId

--- src/main/java/ru/netology/domain/PurchaseItem.java
+++ src/main/java/ru/netology/domain/PurchaseItem.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Data
 public class PurchaseItem {
   private int id;
-  private int productId;
   private String productName;
   private int productPrice;
   private int count;
